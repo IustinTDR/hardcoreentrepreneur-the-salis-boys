f9ed51bc60aafc00c18e248301d26d866f3d10c0
IustinTDR <iustin.p.toader@gmail.com>
2022-04-25 18:51:10 +0200

    git setup 25.4

--- myapp/App.js
+++ myapp/App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+
+
+
+
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      
+
+      <View style={styles.textContainer}>
+        <Text>Hello React Native</Text>
+      </View>
+
     </View>
+
+    
   );
 }
 
@@ -17,4 +30,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  textContainer: {
+    width: '100%',
+    height: '30%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 16,
+    backgroundColor: '#d3d3d3'
+
+
+  }
 });
